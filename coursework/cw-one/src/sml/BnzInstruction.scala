9b62aa1838b2c9ad7fdb78593c52478f758ff274
Andre Braithwaite <andrebwt@gmail.com>
2017-03-02 15:24:43 +0000

    BNZ implementation working. Instruction list is printing still on one line?

---
+++ coursework/cw-one/src/sml/BnzInstruction.scala
@@ -0,0 +1,23 @@
+package sml
+
+class BnzInstruction(label: String, op: String, val op1: Int, val op2: String)
+  extends Instruction(label, op) {
+
+  override def execute(m: Machine) {
+    val value1 = m.regs(op1)
+    val value2 = m.labels.labels.indexOf(op2)
+
+    if (value1 != 0) m.pc = value2
+  }
+
+  override def toString(): String = {
+    super.toString + " " + op1 + " goto " + op2
+  }
+
+
+}
+
+object BnzInstruction {
+  def apply(label: String, op1: Int, op2: String) =
+    new BnzInstruction(label, "bnz", op1, op2)
+}
