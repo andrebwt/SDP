4cb371acbbbb7713381ade6ad28103b70ae988fa
Andre Braithwaite <andrebwt@gmail.com>
2017-03-13 11:36:31 +0000

    Working VirtualMachineParserImpl.

--- coursework/cw-two/src/main/scala/parsers/ProgramParserImpl.scala
+++ coursework/cw-two/src/main/scala/parsers/ProgramParserImpl.scala
@@ -3,8 +3,9 @@ package parsers
 import java.util
 
 import vendor.{Instruction, ProgramParser}
+import bc.{ByteCodeValues, InvalidBytecodeException}
 
-class ProgramParserImpl extends ProgramParser {
+class ProgramParserImpl extends ProgramParser with ByteCodeValues {
 
   def parse(file: String): InstructionList = {
     import scala.io.Source
@@ -24,11 +25,14 @@ class ProgramParserImpl extends ProgramParser {
     for (line <- lines) {
       //var args = new Array[Int]
       val fields = line.split(" ")
-
       val name = fields(0)
-
       val args =  fields.drop(1).map(_.toInt).toVector
-      returnList.add(new Instruction(name, args))
+
+      if (names.contains(fields(0)) && args.forall(a => a.isInstanceOf[Int]))
+        returnList.add(new Instruction(name, args))
+      else
+        throw new InvalidBytecodeException("Invalid bytecode provided")
+
     }
     returnList.toArray.map(_.asInstanceOf[Instruction]).toVector
   }
