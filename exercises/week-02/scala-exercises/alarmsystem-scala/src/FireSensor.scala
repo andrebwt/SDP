543b55543680765955190984bfa193b6f53f0af8
Andre Braithwaite <andrebwt@gmail.com>
2017-04-14 16:34:25 +0100

    Battery drains 10% between each Poll.

--- exercises/week-02/scala-exercises/alarmsystem-scala/src/FireSensor.scala
+++ exercises/week-02/scala-exercises/alarmsystem-scala/src/FireSensor.scala
@@ -2,7 +2,13 @@ import scala.util.Random
 
 class FireSensor extends Sensor {
 
+  var batteryPercent: Int = 100
+
   override def isTriggered: Boolean = {
+    // Drain Battery 10% each Poll
+    batteryPercent -= 10
+
+    // Raise alarm 5% of the time
     val r: Int = Random.nextInt(100) + 1
     if (r < 6) true else false
   }
