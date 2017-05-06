99a8e7561cb4e45afe76003d1db470e833173c8d
Andre Braithwaite <andrebwt@gmail.com>
2017-05-06 17:58:55 +0100

    Renamed test. Added project files to repo.

--- exercises/week-05/src/test/scala/CounterSuite.scala
+++ exercises/week-05/src/test/scala/CounterSuite.scala
@@ -4,9 +4,10 @@ import org.scalatest.junit.JUnitRunner
 
 class CounterSuite extends FunSuite {
 
-  // Checking libraries are imported correctly
+  // Checking libraries imported correctly
   test("one plus one is two")(assert(1 + 1 == 2))
 
-  test("inc without parameter works") (assert(new Counter(10).inc.dec.inc.inc.count == 12))
+  test("inc and dec without parameters work") (assert(new Counter(10).inc.dec.inc.inc.count == 12))
+
 
 }
