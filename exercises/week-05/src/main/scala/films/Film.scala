8873a555bf839bb502f49bd03d873403b8419625
Andre Braithwaite <andrebwt@gmail.com>
2017-05-07 19:00:54 +0100

    Tested provided data works. Updated string comparison in 'isDirectedBy' method to use ==

--- exercises/week-05/src/main/scala/films/Film.scala
+++ exercises/week-05/src/main/scala/films/Film.scala
@@ -2,6 +2,13 @@ package films
 
 class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
 
-  def isDirectedBy(d: Director): Boolean = if (d.equals(director)) true else false
+  def isDirectedBy(d: Director): Boolean = if (d == director) true else false
+
+  def copy(copyName: String = name, copyYearOfRelease: Int = yearOfRelease, copyImdbRating: Double = imdbRating,
+           copyDirector: Director = director): Film = {
+
+  new Film(copyName, copyYearOfRelease, copyImdbRating, copyDirector)
+
+  }
 
 }
