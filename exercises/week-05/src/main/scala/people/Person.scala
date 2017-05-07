8267242eed286286d12263a8fc220e1d0e4fb4a6
Andre Braithwaite <andrebwt@gmail.com>
2017-05-07 16:53:21 +0100

    Implemented companion object.

--- exercises/week-05/src/main/scala/people/Person.scala
+++ exercises/week-05/src/main/scala/people/Person.scala
@@ -1,13 +1,13 @@
 package people
 
-class Person(first: String, last: String)
+class Person(val first: String, val last: String)
 
 object Person {
 
   def apply(name: String): Person = {
 
-    val nameSplit = name.split(" ")
-    new Person(nameSplit(0), nameSplit(1))
+    val parts = name.split(" ")
+    new Person(parts(0), parts(1))
   }
 
 }
--- exercises/week-05/src/test/scala/PersonSuite.scala
+++ exercises/week-05/src/test/scala/PersonSuite.scala
@@ -1,10 +1,24 @@
-import people._
+import people.{Person, _}
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
 class PersonSuite extends FunSuite {
 
+  test("Person created with 2 parameters") {
+    val jd = new Person("John", "Doe")
 
+    assert(jd.first == "John")
+    assert(jd.last == "Doe")
+
+  }
+
+  test("Person created with single parameter") {
+    val jd2 = Person("John Doe")
+
+    assert(jd2.first == "John")
+    assert(jd2.last == "Doe")
+
+  }
 
 }
