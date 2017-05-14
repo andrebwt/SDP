f63edb4e4491a39d2c0ff53ecd27d8f7afb27374
Andre Braithwaite <andrebwt@gmail.com>
2017-05-14 20:36:04 +0100

    Implemented build method.

---
+++ exercises/week-08/src/main/scala/builder/CarDirector.scala
@@ -0,0 +1,16 @@
+package builder
+
+// TODO
+
+case class CarDirector(carBuilder: CarBuilder) {
+  def build = {
+
+    carBuilder.buildBodyStyle
+    carBuilder.buildPower
+    carBuilder.buildEngine
+    carBuilder.buildBreaks
+    carBuilder.buildSeats
+    carBuilder.buildWindows
+    carBuilder.buildFuelType
+  }
+}
