c607f423d25ffde29d3676d9a6dbcd7e83af8a0b
Andre Braithwaite <andrebwt@gmail.com>
2017-05-14 12:19:27 +0100

    Finished implementing XPayToPayDAdapter.

--- exercises/week-07/src/main/scala/xpay/XpayToPayDAdapter.scala
+++ exercises/week-07/src/main/scala/xpay/XpayToPayDAdapter.scala
@@ -1,27 +1,27 @@
 package xpay
 
 // Client calls methods on XpayToPayDAdapter object
-// Adapter translates them into calls on the
+// Adapter translates them into calls on the Xpayimpl
 class XpayToPayDAdapter(xpayObj: Xpay) extends PayD {
 
   def getCustCardNo: String = xpayObj.getCreditCardNo
 
-  def setCustCardNo(custCardNo: String) = ???
+  def setCustCardNo(custCardNo: String) = xpayObj.setCreditCardNo(custCardNo)
 
-  def getCardOwnerName: String = ???
+  def getCardOwnerName: String = xpayObj.getCustomerName
 
-  def setCardOwnerName(cardOwnerName: String) = ???
+  def setCardOwnerName(cardOwnerName: String) = xpayObj.setCustomerName(cardOwnerName)
 
-  def getCardExpMonthDate: String = ???
+  def getCardExpMonthDate: String = xpayObj.getCardExpMonth
 
-  def setCardExpMonthDate(cardExpMonthDate: String) = ???
+  def setCardExpMonthDate(cardExpMonthDate: String) = xpayObj.setCardExpMonth(cardExpMonthDate)
 
-  def getCVVNo: Integer = ???
+  def getCVVNo: Integer = xpayObj.getCardCVVNo.toInt
 
-  def setCVVNo(cVVNo: Integer) = ???
+  def setCVVNo(cVVNo: Integer) = xpayObj.setCardCVVNo(cVVNo.toShort)
 
-  def getTotalAmount: Double = ???
+  def getTotalAmount: Double = xpayObj.getAmount
 
-  def setTotalAmount(totalAmount: Double) = ???
+  def setTotalAmount(totalAmount: Double) = xpayObj.setAmount(totalAmount)
 
 }
