fb88a2d764d3f016bb9f8d9f322a1a3df8de1562
Andre Braithwaite <andrebwt@gmail.com>
2017-05-14 21:06:22 +0100

    Implemented SportsCarBuilder.

--- exercises/week-08/src/main/scala/builder/SportsCarBuilder.scala
+++ exercises/week-08/src/main/scala/builder/SportsCarBuilder.scala
@@ -3,19 +3,26 @@ package builder
 // TODO
 
 object SportsCarBuilder extends CarBuilder {
-  override def buildBodyStyle: Unit = ???
 
-  override def buildPower: Unit = ???
+  val sports = Car("Sports")
 
-  override def buildEngine: Unit = ???
+  override def buildBodyStyle: Unit = sports.setBodyStyle(
+    """External dimensions: overall length (inches): 192.3,
+      |   overall width (inches): 75.5, overall height (inches): 54.2,
+      |   wheelbase (inches): 112.3, front track (inches): 63.7,
+      |   rear track (inches): 64.1 and curb to curb turning circle (feet): 37.7""".stripMargin)
 
-  override def buildBreaks: Unit = ???
+  override def buildPower: Unit = sports.setPower("323 hp @ 6,800 rpm; 278 ft lb of torque @ 4,800 rpm")
 
-  override def buildSeats: Unit = ???
+  override def buildEngine: Unit = sports.setEngine("3.6L V 6 DOHC and variable valve timing")
 
-  override def buildWindows: Unit = ???
+  override def buildBreaks: Unit = sports.setBreaks("Four-wheel disc brakes: two ventilated. Electronic brake distribution.\n   Stability control")
 
-  override def buildFuelType: Unit = ???
+  override def buildSeats: Unit = sports.setSeats("Driver sports front seat with one power adjustments manual height,\n   front passenger seat sports front seat with one power adjustments")
 
-  override def getCar: Car = ???
+  override def buildWindows: Unit = sports.setWindows("Front windows with one-touch on two windows")
+
+  override def buildFuelType: Unit = sports.setFuelType("Petrol 17 MPG city, 28 MPG highway, 20 MPG combined and 380 mi. range")
+
+  override def getCar: Car = sports
 }
